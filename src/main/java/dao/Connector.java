472518653ccb67b1a23632f6323bd3f03f1c25dd
AdamChaplin23 <90810781+AdamChaplin23@users.noreply.github.com>
2021-09-22 16:32:14 -0500

    Connection to db established

--- src/main/java/dao/Connector.java
+++ src/main/java/dao/Connector.java
@@ -1,19 +1,55 @@
 package dao;
 
-import java.sql.DriverManager;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class Connector {
 
-    private Connection connection;
+    /**
+     * Connection connection is static so there is one instance, singleton
+     */
+    static Connection connection = null;
 
-    public void connect() throws ClassNotFoundException, SQLException {
+//    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+//        connect();
+//    }
+
+    /**
+     * Connects the project file to the database
+     * @throws ClassNotFoundException Throws if class isn't found
+     * @throws SQLException
+     */
+    public static void connect() throws ClassNotFoundException, SQLException {
+
         Class.forName("com.mysql.cj.jdbc.Driver");
 
         String url = "jdbc:mysql://localhost:3306/Software";
+        String user1 = "root";
+        String password = "software1!";
 
-        connection = DriverManager.getConnection(url, "uwem", "1234");
+        connection = DriverManager.getConnection(url, user1, password);
 
+//        try {
+//            Statement statement = connection.createStatement();
+//            String newRecord = "SELECT * FROM Flight;";
+//            ResultSet result = statement.executeQuery(newRecord);
+//
+//            while (result.next()) {
+//                int flight = result.getInt("flightID");
+//                int seats = result.getInt("AvailableSeats");
+//
+//                System.out.println(flight + " " + seats);
+//            }
+
+//        } catch (
+//
+//                SQLException e) {
+//            System.out.println("Could not show flights");
+//
+//        }
     }
 }
---
+++ src/test/java/ConnectorTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ConnectorTest {
+
+    @Test
+    public void connectTestSQLException() {
+
+    }
+
+    @Test
+    public void connectTestClassNotFoundException() {
+
+    }
+
+}
