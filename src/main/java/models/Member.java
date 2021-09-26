fc9fffd3557091ebd05fd7ac61919ecf82060493
UwemEkong <uweme6@gmail.com>
2021-09-26 00:05:33 -0500

    Create member model

---
+++ src/main/java/models/Member.java
@@ -0,0 +1,90 @@
+package models;
+
+import java.util.Objects;
+
+public class Member {
+    private String userID;
+    private String paymentInfo;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String userName;
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getPaymentInfo() {
+        return paymentInfo;
+    }
+
+    public void setPaymentInfo(String paymentInfo) {
+        this.paymentInfo = paymentInfo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return Objects.equals(userID, member.userID) && Objects.equals(paymentInfo, member.paymentInfo) && Objects.equals(password, member.password) && Objects.equals(firstName, member.firstName) && Objects.equals(lastName, member.lastName) && Objects.equals(email, member.email) && Objects.equals(userName, member.userName);
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "userID='" + userID + '\'' +
+                ", paymentInfo='" + paymentInfo + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", userName='" + userName + '\'' +
+                '}';
+    }
+}
