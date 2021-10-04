e20dad3ad83a908cdbeef7c561b858f23625ab4c
Niraj Patel <90847730+NirajPatel2021@users.noreply.github.com>
2021-10-04 08:56:34 -0500

    Update DeleteFlight.java

--- src/main/java/servlets/DeleteFlight.java
+++ src/main/java/servlets/DeleteFlight.java
@@ -11,6 +11,7 @@ import models.Flight;
 import models.FlightID;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -24,6 +25,7 @@ public class DeleteFlight extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         int id = Integer.parseInt(request.getParameter("flightID1"));
+
         deleteTheFlight(id);
 
         FlightDao flightDao = new FlightDao();
@@ -35,10 +37,31 @@ public class DeleteFlight extends HttpServlet {
 
     }
 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        List<Flight> resultFlights = getFlights();
+
+        RequestDispatcher rd = request.getRequestDispatcher("deleteFlight.jsp");
+        request.setAttribute("flights", resultFlights);
+        rd.forward(request, response);
+    }
+
     public static void deleteTheFlight(int id){
         FlightDao dao = new FlightDao();
         dao.deleteFlight(id);
 
+    }
+
+    /**
+     *
+     * Helper function to retrieve the list of all available flights
+     *
+     * @return List<Flight>
+     */
+    public static List<Flight> getFlights() {
+        FlightDao dao = new FlightDao();
+        return dao.getAllAvailableFlights();
     }
 
 }
